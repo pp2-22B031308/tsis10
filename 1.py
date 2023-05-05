9112f8f8c1ac6db356033800217dd0b8e23ff7e8
pp2-22B031308 <123051818+pp2-22B031308@users.noreply.github.com>
2023-05-05 16:11:51 +0600

    Update 1.py

--- 1.py
+++ 1.py
@@ -17,7 +17,7 @@ connection = psycopg2.connect(
     host="localhost",
     database="postgres",
     user="postgres",
-    password="123"
+    password="sMile36forever"
 )
 
 cur = connection.cursor()
